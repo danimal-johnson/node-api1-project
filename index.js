7cc4422c9a09ef9baa9fa4e7746b9892453a9db6
Dan Johnson <danimal.johnson@gmail.com>
2019-12-02 12:40:47 -0800

    Server implemented. Added GET /api/users.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// implement your API here
+const express = require('express');
+const db = require ('./data/db.js');
+
+const server = express();
+const port = 4000;
+
+// Middleware
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send({ api: 'Up and running...'})
+});
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(err => {
+      console.log("Error getting /api/users :", err);
+    });
+});
+
+server.listen(port, () => {
+  console.log(`\n ** API running on port ${port} **\n`);
+});
